25274f6bbfb5ade62cc48ccaefcfebee7bd0179a
wlsdnr4675 <wlsdnr4675@gmail.com>
2021-04-20 12:56:39 +0900

    redux start 0420

---
+++ workspaceMS/reduxstudy/src/counter/components/Counter.js
@@ -0,0 +1,38 @@
+import React, {useState, useEffect} from 'react';
+
+const Counter = () => {
+    const [count, setCount] = useState(0);
+
+    const increase = () =>{
+        const add = count + 1;
+
+        setCount(add)
+    }
+    const decrease = () =>{
+        const minus = count - 1;
+
+        setCount(minus)
+    }
+    
+    return (<> 
+    <h2>카운터</h2>
+    
+    <div>
+        <div>
+            <button 
+            aria-label="+"
+            onClick={()=>increase()}>
+                +
+            </button>
+            <span>{count}</span>
+            <button
+            aria-label="-"
+            onClick={()=>decrease()}>
+                -
+            </button>
+        </div>
+    </div>
+     </>);
+}
+ 
+export default Counter;
---
+++ workspaceMS/reduxstudy/src/counter/components/RTKCounter.js
@@ -0,0 +1,8 @@
+import React, {useState, useEffect} from 'react';
+
+const RTKCounter = () => {
+    
+    return (<>  </>);
+}
+ 
+export default RTKCounter;
