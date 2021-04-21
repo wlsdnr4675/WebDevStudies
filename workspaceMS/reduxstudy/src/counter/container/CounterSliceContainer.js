a5223fb0bc19082e86fff7d300742dceb7afa7ff
wlsdnr4675 <wlsdnr4675@gmail.com>
2021-04-21 19:59:33 +0900

    redux toolkit 0421

--- workspaceMS/reduxstudy/src/counter/container/CounterSliceContainer.js
+++ workspaceMS/reduxstudy/src/counter/container/CounterSliceContainer.js
@@ -1,4 +1,5 @@
 
+import { useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux'
 import CounterSlice from '../components/CounterSlice'
 import { increase, decrease } from '../reducer/counter.slice'
@@ -11,10 +12,13 @@ const CounterSliceContainer = () => {
 
     const dispatch = useDispatch()
 
+    const onIncrease = useCallback(()=>dispatch(increase(1)));
+    const onDecrease = useCallback(()=>dispatch(decrease(1)));
+
     return (<>
         <CounterSlice number={number} 
-        onIncrease={() => dispatch(increase(1))} 
-        onDecrease={() => dispatch(decrease(1))}/>
+        onIncrease={onIncrease} 
+        onDecrease={onDecrease}/>
     </>)
 }
 
