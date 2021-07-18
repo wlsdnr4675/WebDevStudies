bd52ce84bf0c072902689754f3ef8d0afd569753
wlsdnr4675 <wlsdnr4675@gmail.com>
2021-07-18 18:58:01 +0900

    without redux 210718

---
+++ withoutRedux_v1-master/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import TodoWrapper from "./todo/TodoWrapper";
+
+function App() {
+  return (
+    <div>
+      <TodoWrapper></TodoWrapper>
+    </div>
+  );
+}
+
+export default App;
